d23460aac06975f51a761158fa6380cb02a2368c
Nakul Bhachawat <70038936+sportbug16@users.noreply.github.com>
2024-05-15 10:29:53 +0000

    commit with sign in and sign up

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
-import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
   return (
-      <div>
-      <p className="text-3xl font-medium text-sky-700">Protected Page</p>
-
-      </div>
-        )
-}
+    <div>
+      <UserButton 
+        afterSignOutUrl='/'
+      />
+    </div>
+  )
+} 
