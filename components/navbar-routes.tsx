6caeed32edfb5c0bbc9040ffd1990da68853858a
Nakul Bhachawat <70038936+sportbug16@users.noreply.github.com>
2024-05-20 17:36:55 +0000

    stripe integration

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useAuth } from "@clerk/nextjs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import { SearchInput } from "./search-input";
-
+import { isTeacher } from "@/lib/teacher";
 export const NavbarRoutes = () => {
+  const { userId } =useAuth();
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
@@ -30,13 +31,13 @@ export const NavbarRoutes = () => {
               Exit
             </Button>
           </Link>
-        ) : (
+        ) : isTeacher(userId) ? (
           <Link href="/teacher/courses">
             <Button size="sm" variant="ghost">
               Teacher mode
             </Button>
           </Link>
-        )}
+        ): null}
         <UserButton afterSignOutUrl="/" />
       </section>
     </>
